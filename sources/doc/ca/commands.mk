84c64d43b8618c0fd98b765d92de31be2b8a8c04
aaloy <aaloy@apsl.net>
2018-09-17 23:17:45 +0200

    traducció commands al català

---
+++ sources/doc/ca/commands.mk
@@ -0,0 +1,38 @@
+——————————————————
+Llista de comandes
+——————————————————
+
+/start - Mostra la informació inicial del bot
+
+/help - Mostra l'ajuda disponible.
+
+/commands - Mostra aquest missatge. Conté informació sobre les comandes disponibles i la seva descripció.
+
+/language - Permet canviar l'idioma dels missatges del bot. Idiomes disponibles actualment:
+	* en (english)
+	* es (spanish).
+	* ca (catalan)
+
+/status - Mostra la configuració actual.
+
+/set_messages - Estableix quants de missatges necessiten publicar els nous usuarios abans que se'ls permeti publicar URLs als missatges.
+
+/set_hours - Estableix quantes han d'esperar els nous usuarios abans que se'ls permeti publicar URLs als missatges.
+
+/call_admins - Crida a tots els administradors del grup.
+
+/call_when_spam - Habilita/deshabilita la notificació als administradors quan es detecta un missatge d'spam.
+
+/users_add_bots - Habilita/deshabilita que els usuaris puguin convidar o afegir bots al grup.
+
+/allow_user - Permet a un usuari publicar URLs als seus missatges.
+
+/disable_user - No permet a un usuari publicar URLs als seus missatges. El marca com a possible spammer.
+
+/enable - Habilita el bot Anti-Spam.
+
+/disable - Deshabilita el bot Anti-Spam.
+
+/version - Mostra la versió del Bot.
+
+/about - Informació sobre el bot.
